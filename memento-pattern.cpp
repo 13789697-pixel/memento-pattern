9651bf5bff0c03b794a20e87de28aafcfba7a1db
Серьоженко Дмитро <13789697@gsuite.duit.edu.ua>
2025-11-20 16:43:54 +0200

    Add files via upload

---
+++ memento-pattern.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+class Memento {
+private:
+	string text;
+public:
+	Memento (const string& t) : text(t) {}
+	string getText() const {
+		return text;
+	}
+};
+class TextBox {
+private:
+	string currentText;
+public: 
+	void SetText(const string& t) {
+		currentText = t;
+		cout << "Textbox set to:" << currentText << endl;
+	}
+	Memento save() const {
+		return Memento(currentText);
+	}
+	void restore(const Memento& m) {
+		currentText = m.getText();
+		cout << "Textbox restored to: " << currentText << endl;
+	}
+};
+int main() {
+	TextBox textbox;
+	Memento snapshot(""); // перший порожній знімок//
+	textbox.SetText("Hello");
+	snapshot = textbox.save();
+	textbox.SetText("world");
+	textbox.restore(snapshot);
+	return(0);
+}
